5fb615373037060db18804a7293f50b41c3aba05
Sochima <34778296+sosochukwuma@users.noreply.github.com>
2021-10-20 03:53:27 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
- * main - Print "Holberton" followed by a new line.
- * Description: You are not allowed to include standard libraries.
- * Return: 0
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	char school[] = "School";
-	int i = 0;
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 
-	while (school[i] != '\0')
-	{
-		_putchar(school[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	return (0);
+return (0);
 }
