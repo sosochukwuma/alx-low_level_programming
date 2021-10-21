cefc2adac113f4d2ff2da12ca730692ab96c0ed0
sosochukwuma <bossan.sb@gmail.com>
2021-10-21 21:02:25 +0100

    lof-t

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,26 +7,19 @@
 
 void print_number(int n)
 {
-	if (n <= 9)
+	unsigned int i;
+
+	if (n < 0)
 	{
-		_putchar((n % 10) + 48);
-	}
-	else if (n >= 10 && n <= 99)
-	{
-		_putchar((n / 10) + 48);
-		_putchar((n % 10) + 48);
-	}
-	else if (n >= 100 && n <= 999)
-	{
-		_putchar((n / 100) + 48);
-		_putchar((n / 10) + 48);
-		_putchar((n % 10) + 48);
+		_putchar('-');
+		i = -n;
 	}
 	else
+		i = n;
+	if (i >= 10)
 	{
-		_putchar((n /1000) + 48);
-		_putchar((n / 100) + 48);
-		_putchar((n / 10) + 48);
-		_putchar((n % 10) + 48);
+		print_number(i / 10);
 	}
+
+	_putchar(i % 10 + '0');
 }
