aab4742482b1a713563e89057c49ccfd12c075be
sosochukwuma <bossan.sb@gmail.com>
2021-10-21 20:54:05 +0100

    lof-t

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,22 @@
 
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 100)
+	for (i = 1; i <= 100; i += 1)
 	{
 		if (i % 3 == 0)
+		{
 			printf("Fizz");
-		if (i % 5 == 0)
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+		else if (i % 5 == 0)
 			printf("Buzz");
-		if ((i % 3 != 0) && (i % 5 != 0))
-			printf("%d", i);
-		if (i < 100)
+		else
+			printf("%i", i);
+		if (i != 100)
 			printf(" ");
-		i++;
 	}
 	printf("\n");
 	return (0);
